3afb4afa84fbde9b2a8d0bea8193879543ba8dd0
Titouan BOULMIER <titouan.boulmier@cea.fr>
2019-07-12 11:09:51 +0200

    update get_version due to restructuration

--- get_version.sh
+++ get_version.sh
@@ -2,8 +2,8 @@
 
 function download() {
     echo "try to get the version: ${1} ..."
-    rm json.hpp
-    wget -c "https://github.com/nlohmann/json/releases/download/v${1}/json.hpp"
+    rm include/nlohmann_json/json.hpp
+    wget -c "https://github.com/nlohmann/json/releases/download/v${1}/json.hpp" -P ./include/nlohmann_json
 }
 
 [[ -n "$1" ]] && download ${1} || echo -e "syntax: ${0} version\nsample: $> ${0} 2.1.2"
